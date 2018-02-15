f8cfc30f80179e0cd4ed5d2b331a520209cd4b19
Alexander <alex.gavriliak@gmail.com>
2018-02-15 02:10:06 +0200

    Added Maven with one Test(for example)

---
+++ src/test/java/MyGameTests.java
@@ -0,0 +1,18 @@
+import junit.framework.Assert;
+import org.junit.Test;
+import ua.kpi.*;
+
+public class MyGameTests {
+
+    @Test
+    public void testGetersSetersModel() {
+        //
+        Model model = new Model();
+        model.setPrevNum(0);
+        model.setNextNum(0);
+        model.setPrevNum(0);
+        int num = model.getNumGuess() + model.getNextNum() + model.getPrevNum();
+        Assert.assertEquals(num, 0);
+    }
+
+}
